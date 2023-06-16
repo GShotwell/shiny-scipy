9261862d566cc6e1e2a00e48892aa778ad848ce7
Gordon Shotwell <gordon.shotwell@posit.co>
2023-06-16 09:34:21 -0300

    Include_files improvements with subfolders

--- helpers.py
+++ helpers.py
@@ -1,4 +1,21 @@
 from pathlib import Path
+import glob
+
+
+def print_file(file_path:str, file_name:str=None):
+    if file_name is not None:
+        print(f"\n## file: {file_name}")
+
+    with open(file_path, "r") as app_file:
+        app_contents = app_file.read()
+        print(app_contents)
+
+
+
+def list_files(path):
+    files = glob.glob(path + '/**', recursive=True)
+    files = [file for file in files if not glob.os.path.isdir(file)]
+    return files
 
 
 def include_shiny_folder(path):
@@ -9,27 +26,19 @@ def include_shiny_folder(path):
     print(header)
 
     # Print contents of app.py
-    app_path = folder_path / "app.py"
-
-    with open(app_path, "r") as app_file:
-        app_contents = app_file.read()
-        print(app_contents)
+    print_file(folder_path / "app.py", None)
 
     exclude_list = ["__pycache__", "app.py"]
 
-    file_paths = [
-        path
-        for path in folder_path.glob("**/*")
-        if not any(exclude in path.parts for exclude in exclude_list)
-    ]
+    files = list_files(path)
+
+    path_list = [string for string in files if not any(exclusion in string for exclusion in exclude_list)]
+
+    file_names = [string.replace(f"{str(folder_path)}/", "") for string in path_list]
 
     # Additional files need to start with ## file:
-    for file_path in file_paths:
-        if file_path.name != "app.py":
-            print(f"\n## file: {file_path.name}")
-            with open(file_path, "r") as file:
-                file_contents = file.read()
-                print(file_contents)
+    for x, y in zip(path_list, file_names):
+        print_file(x, y)
 
     # Finish with the closing tag
     print("```")
